d7f465b528273ed221621599505fafe0b38a91bf
Stefan Wils <stefan@oist.jp>
2007-05-17 03:52:48 +0000

    Activated svn:keywords Id on most files.

---
+++ steps/sim/swiginf/doxygen.hpp
@@ -0,0 +1,18 @@
+////////////////////////////////////////////////////////////////////////////////
+// STEPS - STochastic Engine for Pathway Simulation
+// Copyright (C) 2005-2007 Stefan Wils. All rights reserved.
+//
+// $Id$
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef STEPS_SIM_SWIGINF_DOXYGEN_HPP
+#define STEPS_SIM_SWIGINF_DOXYGEN_HPP 1
+
+///
+/// ...
+///
+
+#endif
+// STEPS_SIM_SWIGINF_DOXYGEN_HPP
+
+// END
