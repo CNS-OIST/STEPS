d2e66a9113b7ea6fd4596b8d6e5eccfe521c861b
Stefan Wils <stefan@oist.jp>
2007-07-24 03:59:39 +0000

    The actual DiffDef object, forgot to commit this in the previous commit.

---
+++ steps/sim/shared/diffdef.cpp
@@ -0,0 +1,57 @@
+////////////////////////////////////////////////////////////////////////////////
+// STEPS - STochastic Engine for Pathway Simulation
+// Copyright (C) 2005-2007 Stefan Wils. All rights reserved.
+//
+// $Id$
+////////////////////////////////////////////////////////////////////////////////
+
+// STL headers.
+#include <cassert>
+#include <string>
+
+// STEPS headers.
+#include <steps/common.h>
+#include <steps/sim/shared/diffdef.hpp>
+#include <steps/sim/shared/statedef.hpp>
+
+USING(std, string);
+
+////////////////////////////////////////////////////////////////////////////////
+
+DiffDef::DiffDef(StateDef * sdef, uint gidx, string const & name)
+: pStateDef(sdef)
+, pFinalSetupFinished(false)
+, pGIDX(gidx)
+, pName(name)
+, pDcst(0.0)
+, pLig(0xFFFF)
+{
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+void DiffDef::setupFinal(void)
+{
+    assert(pFinalSetupFinished == false);
+    pFinalSetupFinished = true;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+void DiffDef::setDcst(double const & d)
+{
+    assert(pFinalSetupFinished == false);
+    pDcst = d;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+void DiffDef::setLig(uint gidx)
+{
+    assert(pFinalSetupFinished == false);
+    pLig = gidx;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+// END
---
+++ steps/sim/shared/diffdef.hpp
@@ -0,0 +1,96 @@
+////////////////////////////////////////////////////////////////////////////////
+// STEPS - STochastic Engine for Pathway Simulation
+// Copyright (C) 2005-2007 Stefan Wils. All rights reserved.
+//
+// $Id$
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef STEPS_SIM_SHARED_DIFFDEF_HPP
+#define STEPS_SIM_SHARED_DIFFDEF_HPP 1
+
+// STL headers.
+#include <string>
+#include <vector>
+
+// STEPS headers.
+#include <steps/common.h>
+
+////////////////////////////////////////////////////////////////////////////////
+
+class StateDef;
+
+class DiffDef
+{
+
+public:
+
+    /// Constructor. 
+    ///
+    DiffDef(StateDef * sdef, uint gidx, std::string const & name);
+
+    StateDef * statedef(void) const
+    { return pStateDef; }
+    
+    uint gidx(void) const
+    { return pGIDX; }
+    
+    std::string const & name(void) const
+    { return pName; }
+
+    ////////////////////////////////////////////////////////////////////////
+    
+    /// Gets called when the definition of all components in the entire state
+    /// has finished.
+    ///
+    /// Currently, this method doesn't really have to do anything.
+    ///
+    void setupFinal(void);
+
+    ////////////////////////////////////////////////////////////////////////
+
+    /// Returns the default diffusion constant of this diffusion rule.
+    ///
+    double dcst(void) const
+    { return pDcst; }
+    
+    /// Set the default diffusion constant for this diffusion rule.
+    ///
+    void setDcst(double const & d);
+
+    ////////////////////////////////////////////////////////////////////////
+    
+    /// Return the global index of the ligand species.
+    ///
+    uint lig(void) const
+    { return pLig; }
+
+    /// Set the ligand of a diffusion rule by its global index.
+    ///
+    void setLig(uint gidx);
+
+private:
+    
+    StateDef *                  pStateDef;
+    
+    /// Keeps track of whether method setupFinal() has already been called.
+    ///
+    bool                        pFinalSetupFinished;
+    
+    uint                        pGIDX;
+    
+    std::string                 pName;
+
+    /// Default (MACROscopic) diffusion constant.
+    ///
+    double                      pDcst;
+    
+    uint                        pLig;
+
+};
+
+////////////////////////////////////////////////////////////////////////////////
+
+#endif
+// STEPS_SIM_SHARED_DIFFDEF_HPP
+
+// END
